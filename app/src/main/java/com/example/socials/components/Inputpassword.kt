dafb065f3912fe2c39c771e576220a3f615ed9ca
IANTSOGod <iantsochristianrazafindrazaka@gmail.com>
2025-08-08 18:47:51 +0300

    adding showing management

--- app/src/main/java/com/example/socials/components/Inputpassword.kt
+++ app/src/main/java/com/example/socials/components/Inputpassword.kt
@@ -4,16 +4,27 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun InputPassword(password: String, onPasswordChange: (String) -> Unit) {
+
+    var isshowed by remember { mutableStateOf(false) }
+
     OutlinedTextField(
         value = password,
         onValueChange = onPasswordChange,
@@ -21,13 +32,14 @@ fun InputPassword(password: String, onPasswordChange: (String) -> Unit) {
         modifier = Modifier.width(300.dp),
         shape = RoundedCornerShape(10.dp),
         singleLine = true,
+        visualTransformation = if (isshowed) VisualTransformation.None else PasswordVisualTransformation(),
         trailingIcon = {
-            IconButton(onClick = {}) {
+            IconButton(onClick = { isshowed = !isshowed }) {
                 Icon(
-                    imageVector = Icons.Default.AccountCircle,
+                    imageVector = if (isshowed) Icons.Default.Close else Icons.Default.Done,
                     contentDescription = null
                 )
             }
-        }
+        },
     )
 }
--- app/src/main/java/com/example/socials/screens/Loginpage.kt
+++ app/src/main/java/com/example/socials/screens/Loginpage.kt
@@ -4,13 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
